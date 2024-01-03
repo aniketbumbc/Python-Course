8d35ed087ff8439f2d1bef49d11be15398933f45
aniket <aniket.umbc@gmail.com>
2024-01-03 07:38:29 +0530

    Basic covering

---
+++ Lesson01/welcome.py
@@ -0,0 +1,25 @@
+line01 = '*******'  # header start
+line02 = '*             *'
+line03 = '*    Welcome      *'  # header end
+
+# Operator in python
+
+age = 22
+
+print('')
+print(age)
+
+# if meaning > 10:
+#     print("Welcome in Right club")
+# else:
+#     print("Look for another club")
+
+# tenary operator
+
+print("Welcome in Right club") if age > 10 else print(
+    "Look for another club")
+
+print(age == 10)
+print(age != 10)
+print(not age)
+print("Hello Bunny") if (age == 222) else print("Something goes wrong")
