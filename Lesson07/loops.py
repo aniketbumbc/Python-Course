64189006db018d7437903cafe653d08ed7ad16d4
aniket <aniket.umbc@gmail.com>
2024-01-06 09:28:24 +0530

    loop

---
+++ Lesson07/loops.py
@@ -0,0 +1,28 @@
+num = 1
+
+# While Loop
+
+# while num <= 10:
+#     print(num)
+#     if num == 6:
+#         break
+#     num += 1
+
+
+# For loop
+
+users = ["Dave", "Jhon", "Sara"]
+habits = ["Eats", "Sleeps", "Work"]
+
+# for name in users:
+#     print(name)
+
+# for x in range(0, 102, 2):
+#     print(x)
+# else:
+#     print("It's over")
+
+
+for user in users:
+    for habit in habits:
+        print(user + " " + habit + ".")
