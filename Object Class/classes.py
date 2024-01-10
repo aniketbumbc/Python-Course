4554edb28af38144d88eed19bf41c35c9b879e9f
aniket <aniket.umbc@gmail.com>
2024-01-10 11:58:21 +0530

    Inheritance

--- Object Class/classes.py
+++ Object Class/classes.py
@@ -17,3 +17,40 @@ my_car = Vehicle("tesla", "2302")
 my_car.get_make_model()
 
 my_car.moves()
+
+
+### Inheritance ####
+
+
+class Airplane(Vehicle):
+    def __init__(self, make, model, faa_id):
+        super().__init__(make, model)
+        self.faa_id = faa_id
+
+    def moves(self):
+        print('Flies Along.....')
+
+
+class Truk (Vehicle):
+    def moves(self):
+        print('slow speed Along.....')
+
+
+class Car (Vehicle):
+    pass
+
+
+vistara = Airplane('india Vistara', 'A623', 'FAA929')
+mack = Truk('india mack', 'B623')
+small_car = Car('Tata', 'Nano')
+
+
+vistara.moves()
+vistara.get_make_model()
+
+
+mack.moves()
+mack.get_make_model()
+
+small_car.moves()
+small_car.get_make_model()
