e6c5b93eebc4deb9909ffe3c2644cf5d6ab201ff
aniket <aniket.umbc@gmail.com>
2024-01-13 08:29:02 +0530

    pckgs steup

---
+++ Pip Lesson/pipPlayground.py
@@ -0,0 +1,7 @@
+# install pckgs
+# python3 -m pip install requests
+#  python3 -m pip list
+# specific pckg  python3 -m pip installl requests==2.30.0
+# uninstall ###  python3 pip uninstall requests
+# Activate virtual env source .venv/bin/activate
+# Deactivate deactivate
