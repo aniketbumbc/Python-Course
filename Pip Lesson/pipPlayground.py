1788d35ca73a11b11e4e354f3f65228b90a4d25f
aniket <aniket.umbc@gmail.com>
2024-01-14 17:46:35 +0530

    weather app

--- Pip Lesson/pipPlayground.py
+++ Pip Lesson/pipPlayground.py
@@ -5,3 +5,24 @@
 # uninstall ###  python3 pip uninstall requests
 # Activate virtual env source .venv/bin/activate
 # Deactivate deactivate
+
+
+import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+
+def current_weather():
+    print("\n**** Get current weather conditions ****\n")
+    city = input("\n Please enter city name: \n")
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={
+        os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+    pprint(weather_data)
+
+
+current_weather()
