f355d6b27825f9207c9af4fe6c55935a260883d1
aniket <aniket.umbc@gmail.com>
2024-01-16 12:58:00 +0530

    mini project compelete

--- small projects/quiz_game.py
+++ small projects/quiz_game.py
@@ -6,22 +6,28 @@ if playing.lower() != "yes":
     quit()
 
 print("Okay!!! Let's Play 🎉")
+score = 0
 
-answer = input("What is meaning of CPU ? \n")
+answer = input("What is meaning of CPU ? \n").lower()
 if answer == "central process units":
     print("Correct !!! ✅")
+    score += 1
 else:
     print("Incorrect !!! ❌")
 
-answer = input("What meaning GPU \n")
+answer = input("What meaning GPU \n").lower()
 if answer == "game process units":
     print("Correct !!! ✅")
+    score += 1
 else:
     print("Incorrect !!! ❌")
 
 
-answer = input("What is RAM meaning ? \n")
+answer = input("What is RAM meaning ? \n").lower()
 if answer == "random access memory":
     print("Correct !!! ✅")
+    score += 1
 else:
     print("Incorrect !!! ❌")
+
+print(f"You got {score} questions correct 🥳🥳🥳")
