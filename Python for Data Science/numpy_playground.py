2eb6183dc4cd4ca55f9f8a00281038e73af744b5
aniket <aniket.umbc@gmail.com>
2024-01-17 06:27:15 +0530

    Numpy library

---
+++ Python for Data Science/numpy_playground.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+np_heights = np.array([23, 55, 66, 50, 34])
+
+# print(np_heights)
+# print(type(np_heights))
+# print(np_heights * 2)
+
+
+list_one = [1, 2, 4, 5,]
+list_second = [6, 7, 8, 9]
+
+merge_list = list_one + list_second
+
+# print(merge_list)
+
+np_list_one = np.array([1, 2, 4, 5,])
+np_list_second = np.array([6, 7, 8, 9])
+
+
+np_merge_list = np_list_one + np_list_second
+
+# print(np_merge_list)
+
+
+print(type(np_heights))
+
+# n dimenssion array
+print(np.__version__)
