c51aa286822442171609dff98da1f71698c999e0
aniket <aniket.umbc@gmail.com>
2024-01-19 07:39:50 +0530

    practice loops

---
+++ small projects/conditional_stm_game.py
@@ -0,0 +1,40 @@
+
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+vowels_word = 'aeiou'
+
+input_word = 'oniket'
+print(input_word[0])
+
+
+if input_word[0] in vowels_word:
+    print("YES")
+else:
+    print("NO")
+
+
+# Element wise difference
+
+L1 = [10, 20, 30, 24, 18]
+L2 = [8, 14, 15, 20, 10]
+L3 = []
+
+
+l = []
+# for i in range(-100, -1, 2):
+#     print(i)
+
+d = {0: 'Fish', 1: 'Bird', 2: 'Mammal'}
+for i in d:
+    print(i)
+
+
+demo = ['VARMA', 'raj', 'Gupta', 'SaNdeeP']
+result = []
+
+for word in demo:
+    result.append(word.capitalize())
+
+
+print(result)
