4b647b3ac1a085a1d5bed121b91de57b4700e1d4
aniket <aniket.umbc@gmail.com>
2024-01-21 08:33:45 +0530

    filter reduce

--- small projects/map_filter_reduce.py
+++ small projects/map_filter_reduce.py
@@ -1,3 +1,4 @@
+from functools import reduce
 countries = ["India", "USA", "Italy", "France"]
 
 # for country in countries:
@@ -62,4 +63,81 @@ def filter_data(x): return x[1] >= 18
 
 get_filter = filter(filter_data, students_data.values())
 
-print(list(get_filter))
+# print(list(get_filter))
+
+
+## Reduce # ************* ########
+
+reduce_list = range(1, 5)
+
+
+def f(x, y): return x*y
+
+
+q = reduce(f, reduce_list)
+
+# print(q)
+
+
+input_list = ['Santa Cruz', 'Santa fe', 'Mumbai', 'Delhi']
+
+
+def count_s(str):
+    counter = 0
+    if (str.startswith('S')):
+        counter += 1
+    return counter
+
+
+count = map(count_s, input_list)
+
+print(sum(count))
+
+multi_arr = [['Ankur', 'Avik', 'Kiran', 'Nitin'],
+             ['Narang', 'Sarkar', 'R', 'Sareen']]
+
+
+first_name_new = multi_arr[0]
+last_name_new = multi_arr[1]
+
+
+# def test_ele(x, y): return x + " " + y
+
+
+name = list(map(lambda x, y: x + " " + y, first_name_new, last_name_new))
+
+# print(name)
+
+
+student = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+           26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+
+
+result_studnet = list(filter((lambda x: x % 5 == 0), student))
+
+# print(result_studnet)
+
+
+shopes = ['soap', 'sharp', 'shy', 'silent', 'ship', 'summer', 'sheep']
+
+
+shopes_filter = list(
+    filter((lambda x: x.startswith('s') and x.endswith('p')), shopes))
+
+
+print(shopes_filter)
+
+
+# reduce
+
+input_list = ['I', 'Love', 'Python']
+
+get_list_result = reduce(lambda x, y: x + ' ' + y, input_list)
+
+print(get_list_result)
+
+find_max = [31, 63, 76, 89]
+
+find_max_result = reduce(lambda x, y: max(x, y), find_max)
+
+print(find_max_result)
