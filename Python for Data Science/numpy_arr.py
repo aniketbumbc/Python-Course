b3ce50160a74be3196933088eed91e6033a6f77a
aniket <aniket.umbc@gmail.com>
2024-01-22 16:48:02 +0530

    num pys methos

---
+++ Python for Data Science/numpy_arr.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+arr = np.ones(5, dtype=int)
+print(arr.dtype)
+
+# print(arr)
+
+
+# multi dim array
+
+multiArr = np.ones((3, 3))
+
+print(multiArr)
+
+zeros = np.zeros(5, dtype=int)
+
+print(zeros)
+
+
+rangeArr = np.arange(4, dtype=int)
+
+print(rangeArr)
