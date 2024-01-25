7e5065da7761191ae5c7d8cc68480a5b50f5ae4f
aniket <aniket.umbc@gmail.com>
2024-01-25 12:51:20 +0530

    method into grp by

--- Python for Data Science/Pandas/ecom_project.py
+++ Python for Data Science/Pandas/ecom_project.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from matplotlib.axes import Subplot
 import numpy as np
 import pandas as pd
@@ -16,21 +17,50 @@ data = pd.read_csv("weatherdata.csv")
 
 # *********  create new columns **********
 
-# print(pd.DatetimeIndex(data["Date"]).year)
+print(pd.DatetimeIndex(data["Date"]).year)
 
 data["Year"] = pd.DatetimeIndex(data["Date"]).year
 data["Month"] = pd.DatetimeIndex(data["Date"]).month
 data["Day"] = pd.DatetimeIndex(data["Date"]).day
 
-data["MaxTemp_F"] = data["MaxTemp"] * 9/5 + 32
+# data["MaxTemp_F"] = data["MaxTemp"] * 9/5 + 32
 
 
 # ******** Lamda functions ***********
 
 
-data["is_rain"] = data["Rainfall"].apply(
-    lambda x: "Rainy" if x > 50 else "No Rain")
-print(data.head())
+# data["is_rain"] = data["Rainfall"].apply(
+#     lambda x: "Rainy" if x > 50 else "No Rain")
+# print(data.head())
 
 
-print(data[data["is_rain"] == "Rainy"])
+# print(data[data["is_rain"] == "Rainy"])
+
+
+# find location where most amount of rainfall
+
+
+# print(data.tail())
+
+data_location = data.groupby(by=["Location"]).mean(numeric_only=True)
+
+# print(data_location.sort_values("Rainfall").tail())
+
+data_month = data.groupby(by=["Month"]).mean(numeric_only=True)
+
+# print(data_month.sort_values("MinTemp").head())
+
+
+# x one row of data frame
+
+
+def wci(x):
+    velocity = x["WindGustSpeed"]
+    minTemp = x["MinTemp"]
+    return (10*sqrt(velocity)-velocity + 10.5)*(33 - minTemp)
+
+
+data["WCI"] = data.apply(wci, axis=1)
+data_grp_month = data.groupby(by=["Month", "Year"]).mean(numeric_only=True)
+
+print(data_grp_month.sort_values(['WCI', 'MaxTemp'], ascending=False).head())
