35fbe57a159a0bf677298f29aa44a044deaf2098
aniket <aniket.umbc@gmail.com>
2024-07-30 22:31:31 +0530

    some practice string and number

--- new-learning/HelloWorld/app.py
+++ new-learning/HelloWorld/app.py
@@ -1,8 +1,38 @@
-print("Hello Python world")
-print('o----------')
-print('||||')
-print('*' * 20)
+# print("Hello Python world")
+# print('o----------')
+# print('||||')
+# print('*' * 20)
 
-price = 10
+# price = 10
+# price = 120
+# rating = 4.9
+# name = "Marsh"
+# is_published = True
 
-print(price)
+name = "Mike"
+age = 20
+is_new_patient = True
+
+print(name)
+print(age)
+print(is_new_patient)
+
+#  Getting input from user
+
+# name = input('What is your name? ')
+# print("hello " + name)
+
+# Type conversion
+# getting typeof variable
+
+# birth_year = input("Birthday year ")
+# print(type(birth_year))
+# age = 2020 - int(birth_year)  # convert into string
+# print(age)
+
+# python string
+
+msg = 'hello how are you'
+print(msg[2])
+print(msg[0:3])
+print(msg[:])
