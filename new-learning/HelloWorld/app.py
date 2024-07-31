f044ffb9e8a0d12341623c258e888f21d33af25d
aniket <aniket.umbc@gmail.com>
2024-07-31 22:18:35 +0530

    simple if else code

--- new-learning/HelloWorld/app.py
+++ new-learning/HelloWorld/app.py
@@ -13,9 +13,9 @@ name = "Mike"
 age = 20
 is_new_patient = True
 
-print(name)
-print(age)
-print(is_new_patient)
+# print(name)
+# print(age)
+# print(is_new_patient)
 
 #  Getting input from user
 
@@ -33,9 +33,52 @@ print(is_new_patient)
 # python string dynamic string
 
 msg = 'hello how are you'
-print(msg[2])
-print(msg[0:3])
-print(msg[:])
+# print(msg[2])
+# print(msg[0:3])
+# print(msg[:])
 end_msg = " I am good"
 
-print(f'{msg}? Yes{end_msg}')
+# print(f'{msg}? Yes{end_msg}')
+
+# python string
+# course = "Python for beginners"
+# print(len(course))
+# print(course.upper())
+# print(course.lower())
+# print(course.find('be'))  # to get index of letter
+# print(course.replace('for', 'to'))
+# print(course)
+# resultText = 'Python' in course  # return boolean value true or False
+# print(resultText)
+
+# python arithmathic paranthisis, exponentiation, multi or division and addition, subtraction
+x = 10+3 * 9
+# print(x)
+y = 20.6
+# print(round(y))
+
+# if statement
+
+is_hot = ""
+is_cold = ""
+
+if is_hot:
+    print("It's hot day")
+    print("It's enjoy your day")
+elif is_cold:
+    print('Wear raincoat')
+else:
+    print('Sleep with AC ON')
+
+# logical operator ie AND, OR , NOT
+
+# convert pound weight or kg weight
+weight = int(input("Please Enter you weight: "))
+pound_or_kg = input("please enter l for pound and k: ")
+
+if pound_or_kg == 'k' or pound_or_kg == 'K':
+    print(f"{weight / 2} Lbs")
+elif pound_or_kg == 'l' or pound_or_kg == 'L':
+    print(f"{weight*2} Kg")
+else:
+    print("You enter wrong char")
