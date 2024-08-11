366800b844781937e31c5ee628a52b676d9714b4
aniket <aniket.umbc@gmail.com>
2024-08-11 08:59:42 +0530

    added template and bind data

--- new-learning/Projects/Django/pyShop/products/views.py
+++ new-learning/Projects/Django/pyShop/products/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello Aniket Welcome Django')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def newProduct(request):
